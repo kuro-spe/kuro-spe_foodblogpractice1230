bb4c22dbc39d730475fb9894755e76791d796b4d
kuro-spe <69505254+kuro-spe@users.noreply.github.com>
2021-12-30 01:37:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var signUpBtn = document.querySelector('.signUp');
+var logInBtn = document.querySelector('.logIn');
+// var msg = document.getElementById('message');
+
+
+//註冊
+function signUpcheck() {
+
+  // model
+  var emailStr = document.querySelector('.email').value;
+  var passwordStr = document.querySelector('.password').value;
+  var account = {}; //輸入的資料，填入空物件
+  account.email = emailStr; //填入的 email
+  account.password = passwordStr; //填入的密碼
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signup', true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  var data = JSON.stringify(account); //因為格式是 JSON，所以要轉字串
+  xhr.send(data);
+  xhr.onload = function () {
+    var callbackData = JSON.parse(xhr.responseText); //因為輸入資料目前是字串，要轉成物件才能使用
+    var str = callbackData.message;
+    if (str == "帳號註冊成功") {
+      alert('Sign Up Success');
+    } else {
+      alert('Account has been used');
+    }
+  }
+}
+
+//登入
+function logIncheck() {
+
+  // model
+  var emailStr = document.querySelector('.email').value;
+  var passwordStr = document.querySelector('.password').value;
+  var account = {}; //輸入的資料，填入空物件
+  account.email = emailStr; //填入的 email
+  account.password = passwordStr; //填入的密碼
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signIn', true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  var data = JSON.stringify(account); //因為格式是 JSON，所以要轉字串
+  xhr.send(data);
+  xhr.onload = function () {
+    var callbackData = JSON.parse(xhr.responseText); //因為輸入資料目前是字串，要轉成物件才能使用
+    var str = callbackData.message;
+    if (str == "登入成功") {
+      alert('Log In Success');
+      location.href="./blog.html"
+    } else {
+      alert('Wrong Account or Password');
+    }
+  }
+
+}
+
+// event
+signUpBtn.addEventListener('click', signUpcheck, false);
+logInBtn.addEventListener('click', logIncheck, false);
